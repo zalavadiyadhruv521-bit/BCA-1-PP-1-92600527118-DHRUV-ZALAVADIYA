24978b77e82484a1de9b1645c43733273331f8a7
zalavadiyadhruv521-bit <zalavadiyadhruv521@gmail.com>
2026-08-21 10:16:46 +0530

    Add files via upload

--- U1P1.C
+++ U1P1.C
@@ -1,18 +1,10 @@
- //print hello word
+//hello world
 #include<stdio.h>
 #include<conio.h>
 
-int main()
+void main()
 {
-  int x,y,z;
-  clrscr();
-  printf("enter the first value : ",x);
-  scanf("%d",&x);
-  printf("enter the second value : ",y);
-  scanf("%d",&y);
-
-  z=x-y;
-  printf("%d+%d=%d",x,y,z);
-  getch();
-  return 0;  x
+	clrscr();
+	printf("Hello world");
+	getch();
 }
